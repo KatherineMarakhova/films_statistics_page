bbd7f3718fd683e0af0e44563120ca3392eab67a
Murka <katherine.marakhova@gmail.com>
2020-08-12 16:03:36 +0300

    add cycles and conditions

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ const personalMovieDB = {
     genres : [],
     private : false
 };
+const films = {};
+for (let i=0; i < numberOfFilms; i++){
+    const a = prompt('Какой фильм смотрели?', ''),
+        b = +prompt('Как вы его оцените (от 0 до 10)', '');
 
-const a = prompt('Какой фильм смотрели?', ''),
-      b = prompt('Как вы его оцените (от 0 до 10)', ''),
-      c = prompt('Какой фильм смотрели?', ''),
-      d = prompt('Как вы его оцените (от 0 до 10)', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b !='' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+if (personalMovieDB.count < 10 && personalMovieDB > 0){
+    console.log("Просмотрено довольно мало фильмов!");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель!");
+} else if (personalMovieDB.count >= 30){
+    console.log("Да вы киноман!");
+} else {
+    console.log('Произошла ошибка!');
+}
+    
 console.log(personalMovieDB);
